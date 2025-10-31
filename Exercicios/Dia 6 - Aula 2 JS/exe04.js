cfd790be348fc42ff210fae2666023da6b366268
Beatriz Monteiro Vieira <beatrizmonteirovieira@outlook.com>
2025-10-31 09:53:19 -0300

    ajuste

---
+++ Exercicios/Dia 6 - Aula 2 JS/exe04.js
@@ -0,0 +1,10 @@
+/*
+Escreva um algoritmo que leia 3 palavras (string), que definem as características de um tipo de animal, 
+segundo o diagrama abaixo, que deve ser lido da esquerda para a direita.
+*/
+
+
+const input = require('readline-sync')
+
+console.log("")
+let desc1 = input.question("")
